81c487309042133b96006231d80bad52555dfd03
celvro <dwlfk2@mst.edu>
2014-07-29 20:50:37 -0500

    Added to run all solutions, zip them, and compare to expected output.

---
+++ code_runner.sh
@@ -0,0 +1,10 @@
+python ./clockangles/solution.py > ClockAngles.txt
+python ./followdirections/solution.py > FollowDirections.txt
+python ./ndrome/solution.py ./ndrome/SampleInput.txt > NDrome.txt
+perl ./replacementgrammar/solution.pl < ./replacementgrammar/sampleinput.txt
+python ./sudoku/sudoku_solver.py ./sudoku/sampleinput.txt > Sudoku.txt
+perl ./textquerymatch/solution.pl < ./textquerymatch/sampleinput.txt
+
+"C:\Program Files\7-Zip\7z.exe" a solutions.zip *.txt
+rm -f *.txt
+CodeCompetitionChecker.exe SampleOutputs.zip solutions.zip
